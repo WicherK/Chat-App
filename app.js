583d6c29ee38f7eb8563681cfb2197b578a5bf73
WicherK <kacper.wicher88@gmail.com>
2022-05-21 18:29:52 +0200

    Added next features
    Added new online list to see who is in the room also repaired mobile view scaling.
    Changed system of creating and joining to the room.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const { authorizedRooms } = require('./js/views.js')
 const app = express()
 const server = http.createServer(app)
 const { Server } = require("socket.io")
@@ -31,13 +32,15 @@ app.use(session({
     secret: secrets.key,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true } //Remember to set it to true!
+    cookie: { secure: false } //Remember to set it to true!
 }))
 
 //Render html pages
 app.get('/', render.mainPage) //Render main page with login and register etc.
 
-app.get('/room/:roomId', render.chatPage) //Getting into room with roomId
+app.get('/room', render.createRoom) //Create your own room
+
+app.get('/join/:roomId', render.joinRoom) //Getting into room with roomId
 
 app.get('/login', render.loginPage) //Render main page with login 
 
@@ -50,23 +53,75 @@ app.post('/login', api.postLogin)
 
 app.post('/register', api.postRegister)
 
+app.get('*', render.errorHandler); //If route doesn't exist go to default page
+
 //Chat
 io.on('connection', (socket) => {
-    socket.on('join', (msg) => {
+    socket.on('join', async (msg) => {
         //Create custom socket's variables
         socket.roomId = msg.roomId
         socket.username = msg.username
 
         socket.join(socket.roomId)
+
+        //If it is first person set their as host
+        io.in(socket.roomId).allSockets().then(res => {
+            if (res.size <= 1 && !socket.isHost)
+                socket.isHost = true
+            else if(res.size >= 1 && socket.isHost)
+                socket.isHost = true
+            else if(res.size > 1 && !socket.isHost)
+                socket.isHost = false
+        })
+
         io.to(socket.roomId).emit('message', '[Server] ' + socket.username + ' has joined to the room.')
+
+        try {
+            let sockets = await io.in(socket.roomId).fetchSockets()
+
+            let clients = []
+
+            for(let i = 0; i < sockets.length; i++)
+            {
+                clients[i] = { username: sockets[i].username, isHost: sockets[i].isHost }
+            }
+
+            io.to(socket.roomId).emit('who', clients)
+        }
+        catch(socketError) {
+            console.log(socketError)
+        }
     })
 
     socket.on('disconnect', (msg) => {          
         //Delete room on last person in room
-        io.in(socket.roomId).allSockets().then(res => {
+        io.in(socket.roomId).allSockets().then(async res => {
             if (res.size == 0) {
-                let thisRoomIndex = render.authorizedRooms.indexOf(socket.roomId)
-                render.authorizedRooms.splice(thisRoomIndex, 1)
+                let thisRoomIndex = authorizedRooms.indexOf(socket.roomId)
+                if(thisRoomIndex != -1)
+                    authorizedRooms.splice(authorizedRooms.indexOf(socket.roomId), 1)
+            }
+            else if(socket.isHost) //Set new leader
+            {
+                try {
+                    let sockets = await io.in(socket.roomId).fetchSockets()
+        
+                    let clients = []
+        
+                    for(let i = 0; i < sockets.length; i++)
+                    {
+                        if(i == 0) //Set host to the first user in room
+                            sockets[i].isHost = true
+
+                        clients[i] = { username: sockets[i].username, isHost: sockets[i].isHost }
+                    }
+        
+                    io.to(socket.roomId).emit('who', clients)
+                    socket.leave(socket.roomId)
+                }
+                catch(socketError) {
+                    console.log(socketError)
+                }
             }
         })
         io.to(socket.roomId).emit('message', '[Server] ' + socket.username + ' has disconnected from the room.')
--- js/views.js
+++ js/views.js
@@ -5,20 +5,28 @@ let authorizedRooms = []
 exports.authorizedRooms = authorizedRooms
 
 exports.mainPage = (req, res) => {
-    if (req.session.isLogged) { //If person is logged we can move on and create their room
-        let id = GetId()
-        authorizedRooms.push(id)
-        res.redirect('/room/' + id)
-    }
+    if (req.session.isLogged) //If person is logged we can move on and create their room
+        res.redirect('/room')
     else //If not back the person to login and register page
         res.render('index')
 }
 
-//This function handle getting into room functionality
-exports.chatPage = (req, res) => {
+//Create room
+exports.createRoom = (req, res) => {
+    if (req.session.isLogged) { //If person is logged in and room is authorized we can put user in room
+        let id = GetId()
+        authorizedRooms.push(id)
+        res.render('index', { isLogged: req.session.isLogged, username: req.session.username, method: 'room', roomId: id })
+    }
+    else
+        res.render('index')
+}
+
+//Join room
+exports.joinRoom = (req, res) => {
     if (req.session.isLogged) { //If person is logged in and room is authorized we can put user in room
         if (authorizedRooms.includes(req.params.roomId))
-            res.render('index', { isLogged: req.session.isLogged, username: req.session.username, roomId: req.params.roomId })
+            res.render('index', { isLogged: req.session.isLogged, username: req.session.username, method: 'join', roomId: req.params.roomId })
         else
             res.redirect('/')
     }
@@ -38,6 +46,10 @@ exports.registerPage = (req, res) => {
         res.redirect('/')
     else
         res.render('register')
+}
+
+exports.errorHandler = (req, res) => {
+    res.redirect('/')
 }
 
 function GetId() {
